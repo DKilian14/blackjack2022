32c9c9a5dca246466c1ffec8fca1e6b6372e65c3
Dan Kilian <dankilian@yahoo.com>
2022-08-02 09:11:09 -0500

    committing

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,420 @@
+// ALL CARD DATA
+const data = [
+    {
+        name: '2 of clubs',
+        picture: "./images/cards/2_of_clubs.png",
+        value: 2
+    },
+    {
+        name: '2 of diamonds',
+        picture: './images/cards/2_of_diamonds.png',
+        value: 2
+    },
+    {
+        name: '2 of hearts',
+        picture: './images/cards/2_of_hearts.png',
+        value: 2
+    },
+    {
+        name: '2 of spades',
+        picture: './images/cards/2_of_spades.png',
+        value: 2
+    },
+    {
+        name: '3 of clubs',
+        picture: './images/cards/3_of_clubs.png',
+        value: 3
+    },
+    {
+        name: '3 of diamonds',
+        picture: './images/cards/3_of_diamonds.png',
+        value: 3
+    },
+    {
+        name: '3 of hearts',
+        picture: './images/cards/3_of_hearts.png',
+        value: 3
+    },
+    {
+        name: '3 of spades',
+        picture: './images/cards/3_of_spades.png',
+        value: 3
+    },
+    {
+        name: '4 of clubs',
+        picture: './images/cards/4_of_clubs.png',
+        value: 4
+    },
+    {
+        name: '4 of diamonds',
+        picture: './images/cards/4_of_diamonds.png',
+        value: 4
+    },
+    {
+        name: '4 of hearts',
+        picture: './images/cards/4_of_hearts.png',
+        value: 4
+    },
+    {
+        name: '4 of spades',
+        picture: './images/cards/4_of_spades.png',
+        value: 4
+    },
+    {
+        name: '5 of clubs',
+        picture: './images/cards/5_of_clubs.png',
+        value: 5
+    },
+    {
+        name: '5 of diamonds',
+        picture: './images/cards/5_of_diamonds.png',
+        value: 5
+    },
+    {
+        name: '5 of hearts',
+        picture: './images/cards/5_of_hearts.png',
+        value: 5
+    },
+    {
+        name: '5 of spades',
+        picture: './images/cards/5_of_spades.png',
+        value: 5
+    },
+    {
+        name: '6 of clubs',
+        picture: './images/cards/6_of_clubs.png',
+        value: 6
+    },
+    {
+        name: '6 of diamonds',
+        picture: './images/cards/6_of_diamonds.png',
+        value: 6
+    },
+    {
+        name: '6 of hearts',
+        picture: './images/cards/6_of_hearts.png',
+        value: 6
+    },
+    {
+        name: '6 of spades',
+        picture: './images/cards/6_of_spades.png',
+        value: 6
+    },
+    {
+        name: '7 of clubs',
+        picture: './images/cards/7_of_clubs.png',
+        value: 7
+    },
+    {
+        name: '7 of diamonds',
+        picture: './images/cards/7_of_diamonds.png',
+        value: 7
+    },
+    {
+        name: '7 of hearts',
+        picture: './images/cards/7_of_hearts.png',
+        value: 7
+    },
+    {
+        name: '7 of spades',
+        picture: './images/cards/7_of_spades.png',
+        value: 7
+    },
+    {
+        name: '8 of clubs',
+        picture: './images/cards/8_of_clubs.png',
+        value: 8
+    },
+    {
+        name: '8 of diamonds',
+        picture: './images/cards/8_of_diamonds.png',
+        value: 8
+    },
+    {
+        name: '8 of hearts',
+        picture: './images/cards/8_of_hearts.png',
+        value: 8
+    },
+    {
+        name: '8 of spades',
+        picture: './images/cards/8_of_spades.png',
+        value: 8
+    },
+    {
+        name: '9 of clubs',
+        picture: './images/cards/9_of_clubs.png',
+        value: 9
+    },
+    {
+        name: '9 of diamonds',
+        picture: './images/cards/9_of_diamonds.png',
+        value: 9
+    },
+    {
+        name: '9 of hearts',
+        picture: './images/cards/9_of_hearts.png',
+        value: 9
+    },
+    {
+        name: '9 of spades',
+        picture: './images/cards/9_of_spades.png',
+        value: 9
+    },
+    {
+        name: '10 of clubs',
+        picture: './images/cards/10_of_clubs.png',
+        value: 10
+    },
+    {
+        name: '10 of diamonds',
+        picture: './images/cards/10_of_diamonds.png',
+        value: 10
+    },
+    {
+        name: '10 of hearts',
+        picture: './images/cards/10_of_hearts.png',
+        value: 10
+    },
+    {
+        name: '10 of spades',
+        picture: './images/cards/10_of_spades.png',
+        value: 10
+    },
+    {
+        name: 'jack of clubs',
+        picture: './images/cards/jack_of_clubs.png',
+        value: 10
+    },
+    {
+        name: 'jack of diamonds',
+        picture: './images/cards/jack_of_diamonds.png',
+        value: 10
+    },
+    {
+        name: 'jack of hearts',
+        picture: './images/cards/jack_of_hearts.png',
+        value: 10
+    },
+    {
+        name: 'jack of spades',
+        picture: './images/cards/jack_of_spades.png',
+        value: 10
+    },
+    {
+        name: 'queen of clubs',
+        picture: './images/cards/queen_of_clubs.png',
+        value: 10
+    },
+    {
+        name: 'queen of diamonds',
+        picture: './images/cards/queen_of_diamonds.png',
+        value: 10
+    },
+    {
+        name: 'queen of hearts',
+        picture: './images/cards/queen_of_hearts.png',
+        value: 10
+    },
+    {
+        name: 'queen of spades',
+        picture: './images/cards/queen_of_spades.png',
+        value: 10
+    },
+    {
+        name: 'king of clubs',
+        picture: './images/cards/king_of_clubs.png',
+        value: 10
+    },
+    {
+        name: 'king of diamonds',
+        picture: './images/cards/king_of_diamonds.png',
+        value: 10
+    },
+    {
+        name: 'king of hearts',
+        picture: './images/cards/king_of_hearts.png',
+        value: 10
+    },
+    {
+        name: 'king of spades',
+        picture: './images/cards/king_of_spades.png',
+        value: 10
+    },
+    {
+        name: 'ace of clubs',
+        picture: './images/cards/ace_of_clubs.png',
+        value: 1
+    },
+    {
+        name: 'ace of diamonds',
+        picture: './images/cards/ace_of_diamonds.png',
+        value: 1
+    },
+    {
+        name: 'ace of hearts',
+        picture: './images/cards/ace_of_hearts.png',
+        value: 1
+    },
+    {
+        name: 'ace of spades',
+        picture: './images/cards/ace_of_spades.png',
+        value: 1
+    },
+]
+
+
+if (window.localStorage.getItem('bank')){  
+}else{
+    window.localStorage.setItem('bank','500')
+}
+let bank = window.localStorage.getItem('bank')
+
+console.log("LocalStorage has a value called bank, worth: ",bank)
+
+// INITIALIZING ALL MANIPULATED DOM ELEMENTS
+const dealerScoreEl = document.querySelector("#dealer-score")
+const dealerHandEl = document.querySelector("#dHand")
+const playerScoreEl = document.querySelector("#player-score")
+const playerHandEl = document.querySelector('#pHand')
+const messageEl = document.querySelector('#message')
+const dealBtnEl = document.querySelector('#deal-button')
+const hitBtnEl = document.querySelector('#hit-button')
+const hitBtnContainerEl = document.querySelector("#hit-button")
+const standBtnEl = document.querySelector('#stand-button')
+
+//INITIALIZING A NEW DECK
+let createNewDeck = () =>{
+    let newDeck = data.map((card)=>{return card})
+    newDeck.push(...newDeck) //append the newDeck with another deck of cards so the playing deck has 2 full decks. 
+    return newDeck
+}
+
+        //--getting a random number (for shuffling deck)
+const getRandomIndex = max =>  Math.floor(Math.random()*max)
+
+        //--shuffling the deck
+const shuffleDeck = (unshuffledDeck) => { // take in an unshuffled deck and shuffle it. 
+
+    let shuffledDeck = [] //create an empty array. this is where the randomly picked cards will go. 
+    while (unshuffledDeck.length>0){ // keep taking a random card from the unshuffled deck until the unshuffled deck is gone. 
+        let cardIndex = getRandomIndex(unshuffledDeck.length) //pick a random card. 
+                                                            //since splice creates an empty lists, you need to pull the dictionary out of the list at index [0]
+        shuffledDeck.push(unshuffledDeck.splice(cardIndex,1)[0]) //pull the random card from the unshuffled deck and put it in the shuffled deck. 
+    }
+    return shuffledDeck
+}
+
+let shuffledDeck = shuffleDeck(createNewDeck())
+
+
+//INITIALIZE PLAYER AND DEALER'S HAND
+let playerCards = []
+let dealerCards = []
+let haveWinner = false 
+
+//CALCULATE SCORE
+let calculateScore = (cards) => {
+    let sum = 0
+    cards.forEach(card => {sum += card.value})
+    //If the sum is less than 12 and there is an ace, change the value of ace from 1 to 11. 
+    if ((sum < 12) && (cards.some((card)=> {return card.value == 1}))){
+        console.log('lets make this 1 an 11.')
+        sum +=10
+    }
+    return sum
+}
+
+
+
+
+// ------------------------------DEAL OUT THE HAND
+dealBtnEl.addEventListener('click',()=>{
+    if (shuffledDeck.length < 12){
+        messageEl.innerText = 'Cards have been shuffled'
+        console.log('small deck')
+        shuffledDeck = shuffleDeck(createNewDeck())
+        
+    }else{
+        messageEl.innerText = ''
+    }
+
+    //refresh the page and the hands. 
+    playerCards = []
+    dealerCards = []
+    haveWinner = false    
+    dealerHandEl.innerHTML = ``
+    playerHandEl.innerHTML = ``
+    haveWinner = false
+
+    //deal 2 cards to the player and the dealer.
+    for (let i = 0; i<2;i++){
+        //pull the card
+        let playerCard = shuffledDeck.pop()
+        let dealerCard = shuffledDeck.pop()
+        //show the image of the card that was pulled on the display. 
+        dealerHandEl.innerHTML += `<img class="card  mr-2" src="${dealerCard.picture}" alt="${dealerCard.name}">`
+        playerHandEl.innerHTML += `<img class="card  mr-2" src="${playerCard.picture}" alt="${dealerCard.name}">`
+        //add the card to the person's hand (used to calculate score. )
+        playerCards.push(playerCard)
+        dealerCards.push(dealerCard)
+        }
+        //display the score
+        dealerScoreEl.innerText = calculateScore(dealerCards)
+        playerScoreEl.innerText = calculateScore(playerCards)
+    
+    }
+)
+
+//-------------------------------PLAYER TAKES A HIT
+hitBtnEl.addEventListener('click',()=>{
+    //if player has 21 or more, don't let them hit.
+    if ((calculateScore(playerCards) >=21) || (haveWinner == true)){
+        console.log('cannot Hit.')
+    //otherwise, the player will draw a card. 
+    }else{
+        let playerCard = shuffledDeck.pop()
+        playerHandEl.innerHTML += `<img class="card mr-2" src="${playerCard.picture}" alt="2 of clubs">`
+        playerCards.push(playerCard)
+        playerScoreEl.innerText = calculateScore(playerCards)
+        //******WIN CONDITION 
+        if (calculateScore(playerCards) > 21){
+            playerScoreEl.innerText += '(BUST)'
+            messageEl.innerText = 'DEALER WINS'
+            haveWinner = true
+        }
+    }
+
+})
+
+
+//-------------------------------PLAYER IS DONE DRAWING. DEALER NOW DRAWS. 
+standBtnEl.addEventListener('click', ()=>{
+    //While the dealer has less than 17, they will take another card. 
+    while ((calculateScore(dealerCards) < 17) && (haveWinner == false)){
+        let dealerCard = shuffledDeck.pop()
+        dealerCards.push(dealerCard)
+        dealerHandEl.innerHTML += `<img class=" card  mr-2" src="${dealerCard.picture}" alt="2 of clubs">`
+        dealerScoreEl.innerText = calculateScore(dealerCards)
+    }
+
+
+
+
+    //*****WIN CONDITIONS
+    if((calculateScore(dealerCards) <= 21) && (calculateScore(dealerCards) > calculateScore(playerCards))){
+        messageEl.innerText = 'DEALER WINS'
+        haveWinner = true
+    }else if ((calculateScore(dealerCards) == 21) && (calculateScore(playerCards)==21)){
+        messageEl.innerText = 'TIE'
+        haveWinner = true
+    }else if (calculateScore(dealerCards) == calculateScore(playerCards)){
+        messageEl.innerText = 'DEALER WINS'
+        haveWinner = true
+    }else if (((calculateScore(playerCards) > calculateScore(dealerCards))|| (calculateScore(dealerCards)> 21)) && (calculateScore(playerCards) <= 21)){
+        messageEl.innerText = 'PLAYER WINS'
+        haveWinner = true
+    }
+        
+})
+
 
